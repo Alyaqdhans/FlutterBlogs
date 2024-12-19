25dc04d7f8f0fabd724ec19d8f4b386e407abecc
xQudo <alyaqdhan690s@gmail.com>
2024-12-20 00:16:58 +0400

    formatted the search file and improved UI

--- lib/search.dart
+++ lib/search.dart
@@ -1,3 +1,4 @@
+import 'package:blogs/widgets/errors/spinner.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import './widgets/blogcard.dart';
@@ -16,35 +17,27 @@ class _SearchState extends State<Search> {
 
   // Build Firestore query based on selected filters
   Stream<QuerySnapshot<Map<String, dynamic>>> getSearchResults() {
-    if (searchQuery.isEmpty) {
-      // If no search query is entered, return an empty stream (no results)
-      return const Stream.empty();
-    }
+    Query<Map<String, dynamic>> query = FirebaseFirestore.instance.collection('blogs')
+      .withConverter<Map<String, dynamic>>(
+        fromFirestore: (snapshot, _) => snapshot.data()!,
+        toFirestore: (blog, _) => blog,
+      );
 
-    // Firestore query reference
-    Query<Map<String, dynamic>> query = FirebaseFirestore.instance
-        .collection('blogs')
-        .withConverter<Map<String, dynamic>>(
-          fromFirestore: (snapshot, _) => snapshot.data()!,
-          toFirestore: (blog, _) => blog,
-        );
-
-    // Apply the filter based on the selected filter
     switch (selectedFilterIndex) {
       case 0: // Title filter
         query = query
-            .where('title', isGreaterThanOrEqualTo: searchQuery)
-            .where('title', isLessThanOrEqualTo: '$searchQuery\uf8ff');
+          .where('title', isGreaterThanOrEqualTo: searchQuery)
+          .where('title', isLessThanOrEqualTo: '$searchQuery\uf8ff');
         break;
       case 1: // Content filter
         query = query
-            .where('contents', isGreaterThanOrEqualTo: searchQuery)
-            .where('contents', isLessThanOrEqualTo: '$searchQuery\uf8ff');
+          .where('contents', isGreaterThanOrEqualTo: searchQuery)
+          .where('contents', isLessThanOrEqualTo: '$searchQuery\uf8ff');
         break;
       case 2: // Username filter
         query = query
-            .where('username', isGreaterThanOrEqualTo: searchQuery)
-            .where('username', isLessThanOrEqualTo: '$searchQuery\uf8ff');
+          .where('username', isGreaterThanOrEqualTo: searchQuery)
+          .where('username', isLessThanOrEqualTo: '$searchQuery\uf8ff');
         break;
       case 3: // Tags filter
         query = query.where('tags', arrayContains: searchQuery);
@@ -53,116 +46,155 @@ class _SearchState extends State<Search> {
         return const Stream.empty();
     }
 
-    // Return the final query stream (filtered based on active filters)
+    // Return the final query stream
     return query.snapshots();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
+
       appBar: AppBar(
         title: const Text('Search'),
         centerTitle: true,
         backgroundColor: Colors.grey[800],
         foregroundColor: Colors.white,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Column(
-          children: [
-            // SearchBar widget (assuming you have a SearchBar widget)
-            SearchBar(
-              controller: _search,
-              backgroundColor: WidgetStateProperty.all(
-                  const Color.fromARGB(255, 230, 244, 255)),
-              leading: const Icon(Icons.search),
-              trailing: [
-                IconButton(
-                  icon: const Icon(Icons.clear),
-                  onPressed: () {
+
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                // SearchBar
+                SearchBar(
+                  controller: _search,
+                  leading: const Icon(Icons.search),
+                  trailing: [
+                    (_search.text.isEmpty)
+                    ? const SizedBox()
+                    : IconButton(
+                      icon: const Icon(Icons.clear),
+                      onPressed: () {
+                        setState(() {
+                          _search.clear();
+                          searchQuery = '';
+                        });
+                      },
+                    ),
+                  ],
+                  hintText: 'Search something...',
+                  padding: WidgetStateProperty.all(const EdgeInsets.only(left: 20, right: 10)),
+                  onChanged: (query) {
                     setState(() {
-                      _search.clear();
-                      searchQuery = '';
+                      searchQuery = query;
+                    });
+                  },
+                ),
+                
+                const SizedBox(height: 20),
+            
+                // Filter Buttons
+                ToggleButtons(
+                  isSelected: [
+                    selectedFilterIndex == 0,
+                    selectedFilterIndex == 1,
+                    selectedFilterIndex == 2,
+                    selectedFilterIndex == 3,
+                  ],
+                  constraints: BoxConstraints(
+                    minWidth: MediaQuery.of(context).size.width / 5,
+                    minHeight: 40,
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                  children: const [
+                    Text("Titles"),
+                    Text("Contents"),
+                    Text("Users"),
+                    Text("Tags"),
+                  ],
+                  onPressed: (int index) {
+                    setState(() {
+                      selectedFilterIndex = index;
                     });
                   },
                 ),
               ],
-              hintText: 'Search here...',
-              padding: WidgetStateProperty.all(
-                  const EdgeInsets.only(left: 20, right: 10)),
-              onChanged: (query) {
-                setState(() {
-                  searchQuery = query;
-                });
+            ),
+          ),
+          
+          // Results
+          Expanded(
+            child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+              stream: getSearchResults(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: Spinner(),
+                  );
+                }
+      
+                if (_search.text.isEmpty) {
+                  return const Center(
+                    child: Padding(
+                      padding: EdgeInsets.all(30),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.search,
+                            size: 100,
+                            color: Colors.grey,
+                          ),
+                      
+                          Text(
+                            'Search to get started',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  );
+                }
+      
+                if (snapshot.data!.docs.isEmpty) {
+                  return const Center(
+                    child: Padding(
+                      padding: EdgeInsets.all(30),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.search_off,
+                            size: 100,
+                            color: Colors.blue,
+                          ),
+                      
+                          Text(
+                            'No Results were found',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  );
+                }
+      
+                return BlogCard(snapshot: snapshot);
               },
             ),
-            const SizedBox(height: 20),
-            // Filter Buttons (Single selection logic)
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: ToggleButtons(
-                isSelected: [
-                  selectedFilterIndex == 0,
-                  selectedFilterIndex == 1,
-                  selectedFilterIndex == 2,
-                  selectedFilterIndex == 3,
-                ],
-                constraints: BoxConstraints(
-                  minWidth: MediaQuery.of(context).size.width / 5,
-                  minHeight: 40,
-                ),
-                borderRadius: BorderRadius.circular(15),
-                children: const [
-                  Text("Titles"),
-                  Text("Contents"),
-                  Text("Users"),
-                  Text("Tags"),
-                ],
-                onPressed: (int index) {
-                  setState(() {
-                    selectedFilterIndex = index; // Update selected filter
-                  });
-                },
-              ),
-            ),
-            const SizedBox(height: 20),
-            // If search query is empty
-            if (searchQuery.isEmpty)
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  'Please enter text to search.',
-                  style: TextStyle(color: Colors.grey, fontSize: 16),
-                ),
-              ),
-            // Results
-            Expanded(
-              child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-                stream: getSearchResults(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return const Center(
-                        child: Icon(
-                      Icons.search_off_outlined,
-                      size: 60,
-                      color: Colors.blue,
-                    ));
-                  }
-
-                  if (snapshot.data!.docs.isEmpty) {
-                    return const Center(
-                      child:
-                          Text('No results found. Try refining your search.'),
-                    );
-                  }
-
-                  // Pass the results directly to BlogCard
-                  return BlogCard(snapshot: snapshot);
-                },
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
